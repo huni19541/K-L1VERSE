0f20150e642b381cf7010636dabb9f1819731afc
olrlobt <317tmdgjs@naver.com>
2024-05-17 15:51:19 +0900

    [Feat] : 추가 시연용 Light Switch 응답

--- backend/UserServer/src/main/java/com/kl1verse/UserServer/domain/user/service/UserService.java
+++ backend/UserServer/src/main/java/com/kl1verse/UserServer/domain/user/service/UserService.java
@@ -7,6 +7,8 @@ import com.kl1verse.UserServer.domain.user.exception.UserException;
 import com.kl1verse.UserServer.domain.user.repository.UserRepository;
 import com.kl1verse.UserServer.domain.user.repository.entity.User;
 import com.kl1verse.UserServer.global.ResponseCode;
+import com.lightswitch.domain.LSUser;
+import com.lightswitch.impl.LightSwitch;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +25,14 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
+    private static LightSwitch lightSwitch = LightSwitch.getInstance();
+
+
+    static {
+        lightSwitch.init("0ca69b1cfd754a1fb78191c941c5c76e", "https://lightswitch.kr");
+    }
+
+
 
     @Transactional
     public void addTenGoal(Integer userId) {
@@ -53,28 +63,34 @@ public class UserService {
         String domain = jwtUtil.extractUserDomainFromExpiredToken(requestToken);
 
         List<CheckEventResDto> eventResDtos = new ArrayList<>();
-        CheckEventResDto checkEventResDto = CheckEventResDto.builder()
-            .name("하나은행 K리그 2024 특별 이벤트")
-            .description("25개 구단 입장권 할인 및 2024 시즌 축덕카드 할인 이벤트")
-            .start_date("2024-01-01")
-            .end_date("2024-12-31")
-            .build();
-        CheckEventResDto checkEventResDto2 = CheckEventResDto.builder()
-            .name("GET THE BALL, OWN THE MOMENT")
-            .description("지금 k리그 경기 득점 공인구와 환희의 순간을 함께 소장하세요!")
-            .start_date("2024-01-01")
-            .end_date("2024-12-31")
-            .build();
-        CheckEventResDto checkEventResDto3 = CheckEventResDto.builder()
-            .name("2024 K 리그 판타지 오픈")
-            .description("점수 시스템 개편, 누적 랭킹과 보상 확대")
-            .start_date("2024-01-01")
-            .end_date("2024-12-31")
+
+        LSUser lsUser = new LSUser.Builder(email)
             .build();
 
-        eventResDtos.add(checkEventResDto);
-        eventResDtos.add(checkEventResDto2);
-        eventResDtos.add(checkEventResDto3);
+        if (lightSwitch.getBooleanFlag("UserEvent", lsUser, false)) {
+            CheckEventResDto checkEventResDto = CheckEventResDto.builder()
+                .name("하나은행 K리그 2024 특별 이벤트")
+                .description("25개 구단 입장권 할인 및 2024 시즌 축덕카드 할인 이벤트")
+                .start_date("2024-01-01")
+                .end_date("2024-12-31")
+                .build();
+            CheckEventResDto checkEventResDto2 = CheckEventResDto.builder()
+                .name("GET THE BALL, OWN THE MOMENT")
+                .description("지금 k리그 경기 득점 공인구와 환희의 순간을 함께 소장하세요!")
+                .start_date("2024-01-01")
+                .end_date("2024-12-31")
+                .build();
+            CheckEventResDto checkEventResDto3 = CheckEventResDto.builder()
+                .name("2024 K 리그 판타지 오픈")
+                .description("점수 시스템 개편, 누적 랭킹과 보상 확대")
+                .start_date("2024-01-01")
+                .end_date("2024-12-31")
+                .build();
+            eventResDtos.add(checkEventResDto);
+            eventResDtos.add(checkEventResDto2);
+            eventResDtos.add(checkEventResDto3);
+        }
+
         return eventResDtos;
     }
 }
